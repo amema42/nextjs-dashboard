413c3dee1e9e43d8a8ecba4192e2352c1c17429f
ani mema <animema@anis-MBP.station>
2024-10-27 23:21:23 +0100

    Done: Chapter 16 - Final Chapter

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,11 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+    title: "Edit Invoice",
+};
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
     const id = params.id;
@@ -11,7 +15,8 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         fetchCustomers(),
     ]);
     if (!invoice) {
-      notFound();
+        notFound();
+    }
     return (
         <main>
             <Breadcrumbs
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import Image from "next/image";
 import { lusitana } from "@/app/ui/fonts";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+    title: "Home",
+};
 export default function Page() {
     return (
         <main className="flex min-h-screen flex-col p-6">
